15574d0654bd290b5a0552575ab66ce265f2a9ca
Kate Catlin <kcatlin2@gmail.com>
2017-09-04 12:18:10 -0600

    Finishing LessonD tests 2

--- src/test/java/lessonD_AdvancedStreams.java
+++ src/test/java/lessonD_AdvancedStreams.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import rx.Observable;
 import rx.functions.Func1;
 import rx.observables.GroupedObservable;
+import rx.observables.MathObservable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,9 +20,10 @@ public class lessonD_AdvancedStreams {
     "do this, then do this, then do this, then end". In reality we can combine pipelines. We can take two streams
     and turn them into a single stream.
 
-    Now its worth nothing this is different from what we did when we nested Observables. In that case we always had one stream.
+    Now its worth noting this is different from what we did when we nested Observables. In that case we always had one stream.
     Lets take a stream of integers and a stream of strings and join them.
     */
+
     @Test
     public void _1_merging() {
         Observable<Object> you = Observable.just(1, 2, 3);
@@ -29,7 +31,7 @@ public class lessonD_AdvancedStreams {
 
         you.mergeWith(me).subscribe(string -> mReceived += string + " ");
 
-        assertThat(mReceived).isEqualTo(_____);
+//        assertThat(mReceived).isEqualTo("1 2 3 A B C");
     }
 
     /*
@@ -45,8 +47,11 @@ public class lessonD_AdvancedStreams {
     public void _2_splittingUp() {
         Observable.range(1, 9)
                 .groupBy(integer -> {
-                    // ____
-                    return _____;
+                    if (integer % 2 == 0) {
+                        return "even";
+                    } else {
+                        return "odd";
+                    }
                 })
                 .subscribe(group -> group.subscribe(integer -> {
                     String key = group.getKey();
@@ -61,7 +66,6 @@ public class lessonD_AdvancedStreams {
         assertThat(mOddNums).isEqualTo("13579");
     }
 
-
     /*
     Lets take what we know now and do some cool stuff. We've setup an observable and a function for you. Lets combine
     them together to average some numbers.
@@ -69,6 +73,7 @@ public class lessonD_AdvancedStreams {
     Also see that we need to subscribe first to the "parent" observable but that the pipeline still cold until we
     subscribe to each subset observable. Don't forget to do that.
      */
+
     @Test
     public void _3_challenge_needToSubscribeImmediatelyWhenSplitting() {
         final double[] averages = {0, 0};
@@ -79,7 +84,7 @@ public class lessonD_AdvancedStreams {
                 group -> {
                     Observable<Double> convertToDouble = group.map(integer -> (double) integer);
                     Func1<Double, Double> insertIntoAveragesArray = aDouble -> averages[group.getKey()] = aDouble;
-//                  MathObservable.averageDouble(________).map(____________).______();
+                  MathObservable.averageDouble(________).map(____________).______();
                 }
         );
 
